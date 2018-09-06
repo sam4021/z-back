1ee962576b8c1f336937eca78f42773e5e390ef9
sam4021 <samsonwandah@gmail.com>
2018-09-06 16:11:36 +0300

    db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
   //database:'mongodb://localhost:27017/zuri',
-  database:'mongodb://wandah:Otero4021@ds245512.mlab.com:45512/zuri-cart',
+  database:'mongodb://wandah:Otero4021&&@ds139585.mlab.com:39585/phones_crm',
+  //database:'mongodb://wandah:Otero4021@ds245512.mlab.com:45512/zuri-cart',
   secter: '7xAJfz6NwBaZe1gaOJgNbrp8NGKZnJciN7TmKX3AQUnCJH7hJhvevCWlrdQ1'
 }
