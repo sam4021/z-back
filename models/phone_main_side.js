4088e94d8c2fcd8454164461f89890c6e2f74f52
sam4021 <samsonwandah@gmail.com>
2018-09-17 15:19:39 +0300

    image

---
+++ models/phone_main_side.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const bcrypt = require('bcryptjs');
+const config = require ('../config/database');
+const timestamps = require('mongoose-timestamp');
+
+//Phone Main Side Schema
+const PhoneMainSideSchema = mongoose.Schema({
+  link: {
+    type: String,
+    default: '#'
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  },
+  sort: {
+    type: Number,
+    default: 0
+  }
+});
+
+PhoneMainSideSchema.plugin(timestamps,  {
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+const PhoneMainSideSchema = module.exports = mongoose.model('phone_main_side', PhoneMainSideSchema);
